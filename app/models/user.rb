9f7402cba2e5f359f58a877c394374cf11cb4105
Jorge Yagüe París <jorgeyp@gmail.com>
2015-02-16 18:32:44 +0100

    Add User model

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User
+  attr_accessor :id
+  attr_accessor :email
+  attr_accessor :username
+  attr_accessor :password
+  attr_accessor :join_date
+  attr_accessor :profile
+  attr_accessor :permission
+  
+end
