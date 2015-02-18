f45e77668cdce84f7bedd03ee1d8bf9dafc3f960
Jorge Yagüe París <jorgeyp@gmail.com>
2015-02-18 12:52:29 +0100

    Add User signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,9 +3,14 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(params[:user])
+    @user = User.new(user_params)
 
     @user.save
     redirect_to @user
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:email, :password)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,27 @@
-using 'savon'
-
 class User
-  attr_accessor :id
-  attr_accessor :email
-  attr_accessor :username
-  attr_accessor :password
-  attr_accessor :join_date
-  attr_accessor :profile
-  attr_accessor :permission
+  include ActiveModel::Validations
+  include ActiveModel::Conversion
+  extend ActiveModel::Naming
+
+  attr_accessor :id, :email, :username, :password, :join_date, :profile, :permission
+
+  validates_presence_of :email, :password
+
+  def initialize(attributes = {})
+    attributes.each do |name, value|
+      send("#{name}=", value)
+    end
+  end
 
   def save
     client = Savon.client(wsdl: 'http://156.35.98.14:8088/folixame/Users.asmx?WSDL')
-
-    client.call(:sign_up, message: {email: @email, password: @password})
+    p client
+    p 'Saving: ' + @email + ' ' + @password
+    response = client.call(:sign_up, message: {email: 'test2', password: 'test2'})
+    p response.body
   end
 
+  def persisted?
+    false
+  end
 end
